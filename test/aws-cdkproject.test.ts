4f6b9005e6946b6de19816b81ba17e7a7f7aba06
AKS-V02 <a.k.s02@outlook.com>
2023-01-23 22:26:20 +0530

    cdk project created

---
+++ test/aws-cdkproject.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkproject from '../lib/aws-cdkproject-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdkproject-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkproject.AwsCdkprojectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
